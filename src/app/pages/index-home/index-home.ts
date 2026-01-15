aa9060aea44a40958b1d009b6e6105403d703320
Lucas Moretti <Lucas.morettic5@gmail.com>
2026-01-15 10:35:26 -0300

    Criação do Front pagina index-home

--- src/app/pages/index-home/index-home.ts
+++ src/app/pages/index-home/index-home.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { Menu } from "../../shared/menu/menu";
+import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-index-home',
-  imports: [Menu],
+  imports: [Menu,
+    MatCardModule],
   templateUrl: './index-home.html',
   styleUrl: './index-home.css',
 })
 export class IndexHome {
 
+  diasTreinados : number = 0;
+  caloriasGastasNaSemana : number = 0;
+  diasTreinadosNaSemana : number = 0;
 }
