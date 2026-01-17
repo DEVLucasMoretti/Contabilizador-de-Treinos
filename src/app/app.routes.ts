13b5c9ff1356855f8f5f6ce8809026a936645985
Lucas Moretti <Lucas.morettic5@gmail.com>
2026-01-16 23:33:46 -0300

    Criação do Front pagina de contabilizar treino

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { IndexHome } from './pages/index-home/index-home';
+import { IndexContabilizarTreino } from './pages/index-contabilizar-treino/index-contabilizar-treino';
+import { IndexRelatorio } from './pages/index-relatorio/index-relatorio';
 
 export const routes: Routes =
 [
 
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: IndexHome, title: 'Home'},
+  {path: 'contabilizarTreino', component: IndexContabilizarTreino, title: 'contabilizar Treino'},
+  {path: 'treinos', component: IndexContabilizarTreino, title: 'contabilizar Treino'},
+  {path: 'relatorio', component: IndexRelatorio, title: 'contabilizar Treino'},
 ];
 
 
--- src/app/pages/index-contabilizar-treino/index-contabilizar-treino.ts
+++ src/app/pages/index-contabilizar-treino/index-contabilizar-treino.ts
@@ -1,11 +1,74 @@
+
+import 'moment/locale/pt-br';
+
+import moment, { Moment } from 'moment';
+
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE
+} from '@angular/material/core';
+
+import {
+  MomentDateAdapter,
+  MatMomentDateModule
+} from '@angular/material-moment-adapter';
+import { Menu } from "../../shared/menu/menu";
+
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'DD/MM/YYYY',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-index-contabilizar-treino',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    Menu
+],
   templateUrl: './index-contabilizar-treino.html',
   styleUrl: './index-contabilizar-treino.css',
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
+  ],
 })
 export class IndexContabilizarTreino {
 
+  caloriasgastas: number | string = '';
+  treinoDoDia: string = '';
+
+  data: Moment = moment();
+
+  constructor(private dateAdapter: DateAdapter<Moment>) {
+    moment.locale('pt-br');
+    this.dateAdapter.setLocale('pt-br');
+  }
 }
