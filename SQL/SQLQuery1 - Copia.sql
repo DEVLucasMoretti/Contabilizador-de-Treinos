85cae156ff371b9e7d8865e70213f5d3b9dbf0e8
Lucas Moretti <Lucas.morettic5@gmail.com>
2026-01-23 23:26:07 -0300

    Criação da Tabela do banco de dados e pensado na logca do programa para iniciar Desenvolvimento da API

--- SQL/SQLQuery1 - Copia.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Treino (
-	Id int IDENTITY(1,1),
-	Quantidade_Caloria decimal(6,2),
-	Treino_Do_Dia varchar(150),
-	Dia_Da_Semana varchar(30),
-	Data Date not null
-)
--- SQL/SQLQuery1.sql
+++ SQL/SQLQuery1.sql
@@ -1,7 +1,9 @@
 CREATE TABLE Treino (
 	Id int IDENTITY(1,1),
+	Data Date not null,
+	Dia_Da_Semana varchar(30) not null, 
 	Quantidade_Caloria decimal(6,2),
 	Treino_Do_Dia varchar(150),
-	Dia_Da_Semana varchar(30),
-	Data Date not null
+
+	CONSTRAINT Id_Treino PRIMARY KEY (Id)
 )
