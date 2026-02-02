077e51e1a38b3342d89f4028fe5e56164ac9fb97
Lucas Moretti <Lucas.morettic5@gmail.com>
2026-02-01 23:04:14 -0300

    Ajustado bug de nao atualizar dados da pagina HOME

--- Angular/AppFitness/src/app/pages/index-home/index-home.ts
+++ Angular/AppFitness/src/app/pages/index-home/index-home.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { Menu } from "../../shared/menu/menu";
-import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
 import { TreinoApi } from '../../services/treino-api';
 import { CommonModule } from '@angular/common';
@@ -16,6 +15,8 @@ import { FormsModule } from '@angular/forms';
 export class IndexHome {
 
   private readonly treinoApi = inject(TreinoApi);
+    private readonly cdr = inject(ChangeDetectorRef);
+
 
 
   diasTreinados : number = 0;
@@ -23,7 +24,9 @@ export class IndexHome {
   diasTreinadosNaSemana : number = 0;
 
 
- constructor() {
+ constructor() {}
+
+ ngOnInit(): void {
   this.construirProgressoSemanal();
   this.construirDiasTreinados();
  }
@@ -37,6 +40,7 @@ export class IndexHome {
       console.log('Progresso da semana:', treinos);
       console.log('Calorias gastas na semana:', this.caloriasGastasNaSemana);
       console.log('Dias treinados na semana:', this.diasTreinadosNaSemana);
+      this.cdr.detectChanges();
     },
     error: (err) => {
       console.error('Erro ao obter o progresso da semana:', err);
@@ -49,6 +53,7 @@ export class IndexHome {
     next: (totalDias) => {
       this.diasTreinados = totalDias;
       console.log('Total de dias treinados:', this.diasTreinados);
+      this.cdr.detectChanges();
     },
     error: (err) => {
       console.error('Erro ao obter o total de dias treinados:', err);
