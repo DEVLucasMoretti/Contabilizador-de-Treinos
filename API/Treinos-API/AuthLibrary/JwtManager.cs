adb6a425a2ecf76696be11d59d35f1efec261a8a
Lucas Moretti <Lucas.morettic5@gmail.com>
2026-02-03 10:41:13 -0300

    Criação JWT API

---
+++ API/Treinos-API/AuthLibrary/JwtManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace AuthLibrary
+{
+    public class JwtManager
+    {
+        private readonly string scretKey;
+        private readonly byte[] secretBytes;
+        private readonly SymmetricSecurityKey signInKey;
+
+        public JwtManager(string secretKey)
+        {
+            this.scretKey = secretKey;
+            secretBytes = Encoding.UTF8.GetBytes(secretKey);
+            this.signInKey = new SymmetricSecurityKey(secretBytes);
+        }
+
+        public string GenerateToken(string username, int expireMinutes = 60)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var credentials = new SigningCredentials(this.signInKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "SistemaTreinoAPI",
+                audience: "AngularAppFitness",
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(expireMinutes),
+                signingCredentials: credentials
+        );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+    }
+}
