cdc11ce28c6ab6cbe956eaaa2eaf92bf6da48385
Lucas Moretti <Lucas.morettic5@gmail.com>
2026-02-04 10:42:21 -0300

    creates atualizados sql

--- SQL/SQLQuery1.sql
+++ SQL/SQLQuery1.sql
@@ -1,3 +1,8 @@
+
+USE BDFIT;
+
+GO
+
 CREATE TABLE Treino (
 	Id int IDENTITY(1,1),
 	Data Date not null,
@@ -6,4 +11,14 @@ CREATE TABLE Treino (
 	Treino_Do_Dia varchar(150),
 
 	CONSTRAINT Id_Treino PRIMARY KEY (Id)
-)
+);
+
+GO
+
+CREATE TABLE Usuario 
+(
+	Id int IDENTITY(1,1),
+	Nome varchar(50) not null,
+	Senha Nvarchar(50) not null,
+	CONSTRAINT pk_Id PRIMARY KEY (Id)
+);
